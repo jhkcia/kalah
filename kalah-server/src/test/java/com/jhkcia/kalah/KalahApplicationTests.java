5486f658d635b296735f43934913389c27966a35
jalal <heidari.jalal74@gmail.com>
2022-06-04 22:30:37 +0430

    change jupiter to junit

--- kalah-server/src/test/java/com/jhkcia/kalah/KalahApplicationTests.java
+++ kalah-server/src/test/java/com/jhkcia/kalah/KalahApplicationTests.java
@@ -1,13 +1,13 @@
 package com.jhkcia.kalah;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class KalahApplicationTests {
+public class KalahApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
--- kalah-server/src/test/java/com/jhkcia/kalah/model/PitTest.java
+++ kalah-server/src/test/java/com/jhkcia/kalah/model/PitTest.java
@@ -1,12 +1,12 @@
 package com.jhkcia.kalah.model;
 
 import org.junit.Assert;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 public class PitTest {
     @Test
     public void testInitialStoneCount() {
-        for (int i = 0; i < 13; i++) {
+        for (int i = 0; i < 14; i++) {
             Pit p = new Pit(i);
             Assert.assertEquals(i, p.getId());
             if (i == 6 || i == 13) {
@@ -19,7 +19,7 @@ public class PitTest {
 
     @Test
     public void testPitType() {
-        for (int i = 0; i < 13; i++) {
+        for (int i = 0; i < 14; i++) {
             Pit p = new Pit(i);
             if (i == 6 || i == 13) {
                 Assert.assertTrue(p.isStore());
